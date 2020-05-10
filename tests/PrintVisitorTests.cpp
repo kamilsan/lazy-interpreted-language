961acba88a875bf8cd6db38d57683bdd18456e16
Kamil Łukasz Przybyła <k.przybyla@stud.elka.pw.edu.pl>
2020-05-10 21:47:06 +0200

    Setup PrintVisitor tests

---
+++ tests/PrintVisitorTests.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <sstream>
+
+#include "Node.hpp"
+#include "PrintVisitor.hpp"
+
+TEST(PrintVisitorTest, NumericLiteral)
+{
+  std::stringstream result;
+  PrintVisitor visitor{result};
+  auto node = std::make_unique<NumericLiteralNode>(10);
+  node->accept(visitor);
+
+  std::string expected{"NumericLiteral: 10\n"};
+  EXPECT_EQ(result.str(), expected);
+}
+
+// To be continued...
